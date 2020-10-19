2683e218a142aa60d3ab9ea46bb1e818f327a800
Hrishikesh Potdar <ruudyroon>
2020-10-19 17:54:33 -0500

    chore: fix start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { PROFILE } from './shared/profile';
-import { StartPage } from "start";
 
 @Component({
   selector: "app-root",
